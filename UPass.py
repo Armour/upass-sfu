642db2b70a5dc5076111c7fe383ed75af2025c0e
Chong Guo <armourg@sfu.ca>
2017-10-24 13:42:55 -0700

    finished script part

--- UPass.py
+++ UPass.py
@@ -1,67 +1,82 @@
-
+#!/usr/bin/env python3
 # coding: utf-8
-
-# In[48]:
-
 
 import requests
 import json
 from lxml import html, etree
 
 r = requests.Session()
-# r.headers.update({'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'})
 
+# get upassbc website
 r1 = r.get('https://upassbc.translink.ca/')
-# print(r1.status_code)
-# print(r1.cookies)
+assert(r1.status_code == 200)
+assert(r1.url == 'https://upassbc.translink.ca/')
 
+# select school, default here sfu
 r2 = r.post('https://upassbc.translink.ca/', data={'PsiId':'sfu'})
-# print(r2.status_code)
-# print(r2.cookies)
-# for history in r2.history:
-#     print(history)
-#     print(history.url)
+assert(r2.status_code == 200)
+assert(r2.url.startswith('https://cas.sfu.ca/cas/login'))
+
+# parse signin form, get all the hidden fields, combined them with username and password in the config file
 tree = html.fromstring(r2.content)
 form = tree.find('.//form[@class="signin-form"]')
-hiddenFields = form.findall('.//input[@type="hidden"]')
-with open('config.json') as config:    
+hidden_fields = form.findall('.//input[@type="hidden"]')
+with open('config.json') as config:
     data = json.load(config)
-for hiddenField in hiddenFields:
-    data[hiddenField.name] = hiddenField.value
-# print(r.cookies)
-
+for hidden_field in hidden_fields:
+    data[hidden_field.name] = hidden_field.value
+# signin post request
 r3 = r.post('https://cas.sfu.ca' + form.action, data=data)
-# print(r3.status_code)
-# print(r3.cookies)
-# for history in r3.history:
-#     print(history)
-#     print(history.url)
+assert(r3.status_code == 200)
+assert(r3.url.startswith('https://idp.sfu.ca/idp/profile'))
+
+# below request r4 and r5 are due to python requests library doesn't load javascript in the webpage,
+# there are javascript to automatically submit those two forms in the webpage, here we mannally do it
 tree = html.fromstring(r3.content)
 form = tree.find('.//form')
 fields = form.findall('.//input')
 data = {}
 for field in fields:
     data[field.name] = field.value
-
 r4 = r.post('https://upassadfs.translink.ca/adfs/ls/', data=data)
-# print(r4.status_code)
-# print(r4.cookies)
-# for history in r4.history:
-#     print(history)
-#     print(history.url)
+assert(r4.status_code == 200)
+assert(r4.url == 'https://upassadfs.translink.ca/adfs/ls/')
+
 tree = html.fromstring(r4.content)
 form = tree.find('.//form')
 fields = form.findall('.//input')
 data = {}
 for field in fields:
     data[field.name] = field.value
-
 r5 = r.post('https://upassbc.translink.ca/fs/', data=data)
-# print(r5.status_code)
-# print(r5.cookies)
-# for history in r5.history:
-#     print(history)
-#     print(history.url)
-print(r5.text)
+assert(r5.status_code == 200)
+assert(r5.url == 'https://upassbc.translink.ca/fs/')
 
+# check if there are new month eligibility need to request
+tree = html.fromstring(r5.content)
+form = tree.find('.//form[@action="/fs/Eligibility/Request"]')
+hidden_fields = form.findall('.//input[@type="hidden"]')
+checkbox_fields = form.findall('.//input[@type="checkbox"]')
+data = {}
+for hidden_field in hidden_fields:
+    data[hidden_field.name] = hidden_field.value
+for checkbox_field in checkbox_fields:
+    data[checkbox_field.name] = 'true'
+if len(checkbox_fields) == 0:
+    print('There is no new request')
+else:
+    # request eligibility
+    print('Request new month eligibility')
+    r6 = r.post('https://upassbc.translink.ca/fs/Eligibility/Request/', data=data)
+    assert(r6.status_code == 200)
+    # print(r6.url)
+    # assert(r6.url.startswith('https://upassbc.translink.ca/'))
 
+    # check if we request successful
+    tree = html.fromstring(r6.content)
+    form = tree.find('.//form[@action="/fs/Eligibility/Request"]')
+    checkbox_fields = form.findall('.//input[@type="checkbox"]')
+    if len(checkbox_fields) == 0:
+        print('Request successful')
+    else:
+        print('Request failed')
